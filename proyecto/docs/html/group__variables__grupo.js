704df025769518acb178f1cc67edf4ec14d32557
Juan Esteban Moscoso <juanmoscoso@unicauca.edu.co>
2025-10-15 21:48:08 -0500

    Subida archivos documentación

---
+++ proyecto/docs/html/group__variables__grupo.js
@@ -0,0 +1,12 @@
+var group__variables__grupo =
+[
+    [ "clo", "group__variables__grupo.html#gae739b5a639d79f73dd4c556eaaec18b2", null ],
+    [ "conteoTempAlta", "group__variables__grupo.html#ga9f4f1908548616eb629643bc9cc956af", null ],
+    [ "humedad", "group__variables__grupo.html#ga34280c09711b9f4f72211e5c467b57bf", null ],
+    [ "luz", "group__variables__grupo.html#gaa22435b55a8b84bf16cf00696cefbad1", null ],
+    [ "M", "group__variables__grupo.html#ga036e77f98fbf5ae1d19ec8dc38433c03", null ],
+    [ "tempA", "group__variables__grupo.html#gac3170cbfa9a8c8b0c3d2ee9d0765c293", null ],
+    [ "tempR", "group__variables__grupo.html#gab35f43c7567f70886ccbc1870761fd13", null ],
+    [ "tiempoMonitorVencido", "group__variables__grupo.html#ga5d521eacd7a2fee907e0573ddc6f34ee", null ],
+    [ "vel_ar", "group__variables__grupo.html#ga23d3d189bb77135c9088b454b5a1e457", null ]
+];
